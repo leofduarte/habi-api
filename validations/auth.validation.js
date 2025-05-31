b9ccf885db6a027f34360e667cde1e94136e06db
leofduarte <leandroduarte@ua.pt>
2025-05-31 13:39:18 +0100

    feat: update registerSchema to include optional timezone fields

--- validations/auth.validation.js
+++ validations/auth.validation.js
@@ -4,9 +4,10 @@ const registerSchema = z.object({
     email: z.string().email(),
     password: z.string().min(6),
     firstName: z.string(),
-    lastName: z.string()
+    lastName: z.string(),
+    timezone_offset: z.number().optional(),
+    timezone_name: z.string().optional()
 });
-
 const loginSchema = z.object({
     email: z.string().email(),
     password: z.string()
