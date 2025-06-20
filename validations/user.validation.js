0a6884563c579a470864ada966e469e92c8d8caf
mrm277 <107406775+mrm277@users.noreply.github.com>
2025-06-20 18:48:43 +0100

    refactor:  adding  sponsor_special_mission field to user.validation

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -1,4 +1,4 @@
-const { z } = require('zod');
+const { z } = require('zod')
 
 // const userIdSchema = z.object({
 //     id: z.number().int().positive("User ID must be a positive integer"),
@@ -8,23 +8,33 @@ const { z } = require('zod');
 //     email: z.string().email("Invalid email format"),
 // });
 
-const updateUserSchema = z.object({
-    email: z.string().email("Invalid email format").optional(),
-    password: z.string().min(6, "Password must be at least 6 characters long").optional(), 
+const updateUserSchema = z
+  .object({
+    email: z.string().email('Invalid email format').optional(),
+    password: z
+      .string()
+      .min(6, 'Password must be at least 6 characters long')
+      .optional(),
     firstName: z.string().optional(),
-    lastName: z.string().optional(), 
-}).refine(data => Object.keys(data).length > 0, {
-    message: "At least one field to update is required",
-});
+    lastName: z.string().optional(),
+    sponsor_special_mission: z.boolean().optional()
+  })
+  .refine((data) => Object.keys(data).length > 0, {
+    message: 'At least one field to update is required'
+  })
 
 const changePasswordSchema = z.object({
-    currentPassword: z.string().min(6, "Current password must be at least 6 characters long"), 
-    newPassword: z.string().min(6, "New password must be at least 6 characters long"),
-});
+  currentPassword: z
+    .string()
+    .min(6, 'Current password must be at least 6 characters long'),
+  newPassword: z
+    .string()
+    .min(6, 'New password must be at least 6 characters long')
+})
 
 module.exports = {
-    // userIdSchema,
-    // userEmailSchema,
-    updateUserSchema,
-    changePasswordSchema,
-};
+  // userIdSchema,
+  // userEmailSchema,
+  updateUserSchema,
+  changePasswordSchema
+}
